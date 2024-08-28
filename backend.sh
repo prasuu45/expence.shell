963615138b4023b3a5a9d677b8e39af97ca712a6
prasuu45 <prasuuchow93@gmail.com>
2024-08-28 13:47:19 +0530

    expence

--- backend.sh
+++ backend.sh
@@ -67,7 +67,7 @@ unzip /tmp/backend.zip &>>$LOG_FILE
 VALIDATE $? "Extracting backend application code"
 
 npm install &>>LOG_FILE
- cp /home/ec2-user/expence.shell /etc/systemd/system/backend.service
+cp /home/ec2-user/expence.shell/backend.service /etc/systemd/system/backend.service
 
 dnf install mysql -y &>>$LOG_FILE
 VALIDATE $? "Installing MySQL Client"
